c047bc271f094b97c5a42dbcb0c765aa21c22fa0
Brightness-Balidy <balidyjacinto@gmail.com>
2023-02-09 01:28:49 +0200

    navbar com bootstrap sem logotipo

---
+++ resources/views/site/login.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/login.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <style>
+    
+    </style>
+</head>
+<body>
+
+
+    <nav class="navbar bg-light">
+        <div id="main" class="container ">
+            <a href="" class="navbar-brand"> CFM</a>
+            <span class="navbar-text">portuguese</span>
+        </div>
+    </nav>
+       
+    
+</body>
+</html>
