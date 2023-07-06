93d0f868d956f66b734e79e58a60bee3d5f5f1c7
Brightness-Balidy <balidyjacinto@gmail.com>
2023-07-06 18:15:47 +0200

    seeder criado para categorias

--- database/seeders/CategoriasSeeder.php
+++ database/seeders/CategoriasSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\categoria;
 class CategoriasSeeder extends Seeder
 {
     /**
@@ -14,6 +14,6 @@ class CategoriasSeeder extends Seeder
      */
     public function run()
     {
-        
+        categoria::factory(5)->create();
     }
 }
