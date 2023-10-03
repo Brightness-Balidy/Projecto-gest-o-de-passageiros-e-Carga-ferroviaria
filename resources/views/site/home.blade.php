00a10c34bb7d6d5d7dbceac4943e5d0727bce196
Brightness-Balidy <balidyjacinto@gmail.com>
2023-10-03 14:06:27 +0200

    primeira secction alinhada

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -19,32 +19,37 @@
       width: 100%;
       height: 40%;
     }
+    .icones{
+      padding-left: 35%;
+    }
     
 </style> 
 <!--a row que vai conter os elementos centrais-->
 <!--cada elemento deve estar dentro da sua propria col-->
 <div class= "row container rowCont" id="rowCont" >
   <div class="row">
-    <div class="col s4"><i class="material-icons large">flash_on</i><p>este sistema é extremamente fácil de usar, e é de tamanha eficiencia</p></div>
-    <div class="col s4"><i class="material-icons large">people</i><p>é Um sistema que foi desenhado para uso entre vários usuários da equipe de trabalho</p></div>
-    <div class="col s4"><i class="material-icons large">work</i><p>Tem o objectivo também de ajudar a manter o fluxo de trabalho constante para que não haja situações em que o trabalho deve ser interompido</p></div>
+    <div class="col s4"><i class="material-icons large icones">flash_on</i><p class="center-align">este sistema é de uso fácil e extremamente intuitivo, e permite obter eficiencia no trabalho</p></div>
+    <div class="col s4"><i class="material-icons large icones">people</i><p>é Um sistema que foi desenhado para uso entre vários usuários da equipe de trabalho</p></div>
+    <div class="col s4"><i class="material-icons large icones">work</i><p>Tem o objectivo também de ajudar a manter o fluxo de trabalho constante para que não haja situações em que o trabalho deve ser interompido</p></div>
   </div>
-
+<!--uma nova row deve ser criada para fazer uma fila de elementos, cada elemento deve estar dentro da sua propria col-->
   <div class="row">
   @foreach ($destinos as $destino)
-  <div class="col s12 m4">
-      <div class="card">
-          <div class="card-image">
-            <img src="{{$destino->imagem}}">
-            
-            <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
-          </div>
-          <div class="card-content">
-              <span class="card-title">{{$destino-> nome}}</span>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ex sed in quaerat ducimus? Consectetur ipsa asperiores adipisci dolore. Perspiciatis..</p>
-          </div>
-        </div>
+  <div class="col s4">
+  <div class="card bg-dark">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="{{$destino->imagem}}">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">{{$destino->nome}}<i class="material-icons right">more_vert</i></span>
+      <p><a href="#">This is a link</a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">{{$destino->nome}}<i class="material-icons right">close</i></span>
+      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+    </div>
   </div>
+</div>
   @endforeach 
 </div>
   <div class="col ">
